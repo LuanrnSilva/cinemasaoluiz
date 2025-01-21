ee4540f0381cce7af4b49b611f1a0331146778f5
luan.nascimento <luan.nascimento@segsat.com>
2025-01-21 15:03:26 -0300

    Created project

---
+++ src/test/java/br/com/luandev/cinemasaoluiz/CinemasaoluizApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luandev.cinemasaoluiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemasaoluizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
